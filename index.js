c8a6e4c0b18410a44d95ba8d2bc1e4aa52fc3f77
tikrack <tikrackcode@gmail.com>
2025-03-28 11:45:16 +0330

    remove(sw): remove service worker

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ import TelJs from "/TelJs.js";
 
 let telegram = new TelJs("7625182592:AAGX0jkcydTx7NG2e2X3zkUvnOMmK5DeS9s", "-1002192270366")
 
-telegram.send("its a test").then((result) => {
+telegram.send(`<b> hello </b>`).then((result) => {
     console.log(result);
-}).catch((err) => {
-    
-});
+})  
